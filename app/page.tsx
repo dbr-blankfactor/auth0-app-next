dc11f5f31f8cb658773f3c5f78e744967aa03287
Dan <danyeis.bravo@blankfactor.com>
2025-05-17 11:24:00 -0500

    refactor: simplify DashboardPage and remove unused auth0 middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useUser } from "@auth0/nextjs-auth0";
 import { redirect } from "next/navigation";
 
 export default function DashboardPage() {
-  const { user } = useUser();
 
   return (
     <div className="flex h-screen bg-[#1a2233] text-white">
-      {!user ? (
-        <div className="h-screen w-full flex justify-center items-center">
-          <Button>
-            <a href="/auth/login">Login</a>{" "}
-          </Button>
-        </div>
-      ) : (
-        <>{redirect("/home")}</>
-      )}{" "}
+      {redirect("/home")}
     </div>
   );
 }
